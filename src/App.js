b4689775ad1c7c7b8eca34e38d3dff1411156998
MCappe <melissacappe@gmail.com>
2020-12-07 15:23:23 -0500

    started on components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
 import './App.css';
+import axios from 'axios';
+import Header from './Components/Header.js';
 import { Component } from 'react';
 
+
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      tvshow: []
+    }
+
+
+  }
+
+  componentDidMount() {
+    axios({
+      url: 'http://api.tvmaze.com/singlesearch/shows',
+      method: 'GET',
+      responseType: 'json',
+      params: {
+        q: 'friends'
+      }
+    }).then((res) => {
+      console.log(res);
+      
+    }) 
+    axios({
+      url: 'https://api.yelp.com/v3/businesses/search',
+      method: 'GET',
+      responseType: 'json',
+      params: {
+        latitude: 43.22470215689218,
+        longtitude: -79.87237315283824,
+        term: 'food'
+      },
+      header: {
+        Authorization: 'Bearer _ySZyzuihg1O-lyVqCt2-yZN4fuew1KxFLk_27F9XwOYREu5e5Q_mzxfbqBOsAWioxGNmcPeZfUsspraBGysxtP66PZ7KRsC62o6oElSq4iWivyUP4zpB4IizQnLX3Yx'
+      }
+    }).then((res) => {
+      console.log(res);
+      
+    })
+    
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>Binge Fest!</h1>
+        <Header />
       </div>
     );
   }
---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <header>
+                <h1>Binge Fest</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita quod fugiat nesciunt beatae nemo delectus exercitationem aperiam quae est cum.</p>
+                <a href="#">Enter</a>
+            </header>
+        )
+    }
+}
+
+export default Header;
